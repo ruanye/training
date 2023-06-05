74379a60591e21473591911b6981dcb193e71ffa
阮野 <ruanye@88.com>
2023-06-05 19:18:48 +0800

    a

---
+++ docs/.vitepress/config.ts
@@ -0,0 +1,50 @@
+import { defineConfig } from 'vitepress';
+
+// refer https://vitepress.vuejs.org/config/introduction for details
+export default defineConfig({
+  lang: 'zh-cn',
+  title: '蓝桥云课',
+  description: 'Vite & Vue powered static site generator.',
+
+  themeConfig: {
+    nav: [
+      { text: '开发流程', link: '/01' },
+
+      // {
+      //   text: 'Dropdown Menu',
+      //   items: [
+      //     { text: 'Item A', link: '/item-1' },
+      //     { text: 'Item B', link: '/item-2' },
+      //     { text: 'Item C', link: '/item-3' },
+      //   ],
+      // },
+
+      // ...
+    ],
+
+    sidebar: [
+      {
+        text: '工作流程',
+        items: [
+          { text: '开发流程', link: '/01' },
+          { text: '团队成员及分工', link: '/02' },
+        ],
+      },
+      {
+        text: 'vue2.x 构建大型项目',
+        items: [
+          { text: 'Vue2.x 中构建大型项目', link: '/03' },
+          { text: 'Vue2.x 中构建大型项目', link: '/04' },
+        ]
+      },
+      {
+        text: '项目制作',
+        items: [
+          { text: '首页', link: '/05' },
+          { text: 'Vue2.x 中构建大型项目', link: '/04' },
+        ]
+      }
+      
+    ],
+  },
+});
---
+++ docs/index.md
@@ -0,0 +1,15 @@
+---
+layout: home
+
+hero:
+  name: 前端项目实训课
+  text: 项目驱动式教学
+  tagline:  构建交互式的单页面应用程序，Vue 拆分独立的组件
+  actions:
+    - theme: brand
+      text: Get Started
+      link: /01
+    - theme: alt
+      text: View on GitHub
+      link: https://github.com/vuejs/vitepress
+---
