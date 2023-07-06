09ed2af8d1ceaeceb0537794cc87dbbe14d4acaa
ruanye <runaye@88.com>
2023-07-06 19:22:58 +0800

    Auto commit on branch main: Auto commit on branch main: Auto commit on branch main: Auto commit on branch main: Auto commit on branch main: v

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -12,15 +12,6 @@ export default defineConfig({
     nav: [
       { text: '开发流程', link: '/01' },
 
-      {
-        text: 'Dropdown Menu',
-        items: [
-          { text: 'Item A', link: '/item-1' },
-          { text: 'Item B', link: '/item-2' },
-          { text: 'Item C', link: '/item-3' },
-        ],
-      },
-
       // ...
     ],
 
